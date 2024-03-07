3c992057ca64313a32d9691a42875600d7466e26
Bo Yuan <boyuxa@syntronic.com>
2024-03-07 18:58:57 -0500

    first commit. husky setup and node init.

---
+++ infrastructure/variables.tf
@@ -0,0 +1,10 @@
+# Define config variables
+variable "labelPrefix" {
+  type        = string
+  default = "yuan0037"
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  default = "canadacentral"
+}
