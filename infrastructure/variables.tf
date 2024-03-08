8dcab5fbd2e6c416c99b3bd30f97838fea2bc9fb
Bo Yuan <boyuxa@syntronic.com>
2024-03-07 21:33:26 -0500

    test commit

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,11 +1,11 @@
 # Define config variables
 variable "labelPrefix" {
   type        = string
-  default = "yuan0037"
+  default     = "yuan0037"
   description = "Your college username. This will form the beginning of various resource names."
 }
 
 variable "region" {
-  type = string
+  type= string
   default = "canadacentral"
 }
